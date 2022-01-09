328b8f93b0f38ec49245786398ae09b3c385fd91
Georgy Komarov <jubnzv@gmail.com>
2022-01-09 17:42:40 +0300

    passes: Implement timeout for script execution

--- src/passes/mod.rs
+++ src/passes/mod.rs
@@ -3,8 +3,11 @@ pub mod pass_imports;
 use super::app::App;
 use crate::treesitter;
 use std::fmt;
+use std::process::{Child, Command, ExitStatus};
 use std::rc::Rc;
+use std::time::Duration;
 use tree_sitter::Node as TSNode;
+use wait_timeout::ChildExt;
 
 /// Outcomes for test frameworks defined at POSIX 1003.3.
 /// See: POSIX 1003.3: 1.4 A POSIX compliant test framework.
@@ -65,17 +68,39 @@ pub trait Pass<'a> {
             Err(_) => return Err("Cannot write to file".to_string()),
             Ok(_) => (),
         };
-        let result = run_command(self.app().script.as_str(), vec![&temp_file]);
+        let result = run_command(
+            self.app().script.as_str(),
+            self.app().timeout,
+            vec![&temp_file],
+        );
         log::debug!("File: {} Result: {}", &temp_file, &result);
         Ok((result, source.to_string()))
     }
 }
 
+/// Wait wraps over `wait` function implementing timeout logic.
+fn wait(child: &mut Child, timeout: Option<u32>) -> std::io::Result<ExitStatus> {
+    match timeout {
+        None => child.wait(),
+        Some(timeout) => {
+            let time_sec = Duration::from_secs(timeout as u64);
+            match child.wait_timeout(time_sec).unwrap() {
+                Some(s) => Ok(s),
+                None => {
+                    // Time out. Kill the child and return status code which will contain the
+                    // error.
+                    child.kill().unwrap();
+                    Ok(child.wait().unwrap())
+                }
+            }
+        }
+    }
+}
+
 /// Executes the given shell command and returns TestOutcome::PASS if it returns 0 return code.
-/// TODO: Add a timeout.
-fn run_command(script: &str, args: Vec<&str>) -> TestOutcome {
-    if let Ok(mut child) = std::process::Command::new(script).args(&args).spawn() {
-        match child.wait() {
+fn run_command(script: &str, timeout: Option<u32>, args: Vec<&str>) -> TestOutcome {
+    if let Ok(mut child) = Command::new(script).args(&args).spawn() {
+        match wait(&mut child, timeout) {
             Ok(s) => {
                 if s.success() {
                     TestOutcome::PASS
